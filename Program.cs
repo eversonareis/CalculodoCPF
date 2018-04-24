e803038c48ff8c303582a1f77da88876e61b25dd
EVERSON ALVES DOS REIS <everson.areis@senacsp.edu.br>
2018-04-24 11:20:58 -0300

    Estrutura e Variáveis

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace calculodoCPF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpfcliente;
+            string cpf9;
+            string cpf10;
+            int contagem1=10;
+            int contagem2=11;
+            int resto=0;
+            int rs1 = 0;
+            int rs2 = 0;
+            
+        }
+    }
+}
